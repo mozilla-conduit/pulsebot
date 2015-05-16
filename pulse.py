45edfeea8eb914be051bf490136a48c67af1a1dc
Mike Hommey <mh@glandium.org>
2015-05-16 09:37:02 +0900

    Move pulse interaction in a separate module

--- pulse.py
+++ pulse.py
@@ -5,15 +5,13 @@
 import json
 import re
 import requests
-import socket
 import threading
 import time
 import traceback
 import willie
 from collections import defaultdict
-from kombu import Exchange
-from mozillapulse import consumers
 from Queue import Queue, Empty
+from pulsebot import pulse
 
 # TODO: Remove debugging messages to owner.
 
@@ -174,10 +172,12 @@ class PulseListener(object):
         if bot.config.has_option('bugzilla', 'pulse'):
             self.bugzilla_branches = bot.config.bugzilla.get_list('pulse')
 
-        self.auth = {
-            'user': bot.config.pulse.user,
-            'password': bot.config.pulse.password,
-        }
+        self.pulse = pulse.PulseListener(
+            bot.config.pulse.user,
+            bot.config.pulse.password,
+            bot.config.pulse.applabel
+            if bot.config.has_option('pulse', 'applabel') else None
+        )
 
         if bot.config.has_option('pulse', 'channels'):
             for chan in bot.config.pulse.get_list('channels'):
@@ -189,35 +189,15 @@ class PulseListener(object):
         if bot.config.has_option('pulse', 'max_checkins'):
             self.max_checkins = bot.config.pulse.max_checkins
 
-        if not bot.config.has_option('pulse', 'applabel'):
-            bot.config.add_section('pulse')
-            # Let's generate a unique label for the script
-            try:
-                import uuid
-                bot.config.pulse.applabel = 'pulsebot-%s' % uuid.uuid4()
-            except:
-                from datetime import datetime
-                bot.config.pulse.applabel = 'pulsebot-%s' % datetime.now()
-
-        self.applabel = bot.config.pulse.applabel
-
         if self.config:
-            self.queue = Queue(42)
             self.bugzilla_queue = Queue(42)
             self.reporter_thread = threading.Thread(target=self.change_reporter)
-            self.listener_thread = threading.Thread(target=self.pulse_listener)
             self.bugzilla_thread = threading.Thread(target=self.bugzilla_reporter)
             self.reporter_thread.start()
-            self.listener_thread.start()
             self.bugzilla_thread.start()
 
     def change_reporter(self):
-        while not self.shutting_down:
-            try:
-                rev, branch, revlink, data = self.queue.get(timeout=1)
-            except Empty:
-                continue
-
+        for rev, branch, revlink, data in self.pulse:
             repo = REVLINK_RE.sub('', revlink)
             pushes_url = '%s/json-pushes?full=1&changeset=%s' \
                 % (repo, rev)
@@ -358,91 +338,10 @@ class PulseListener(object):
                     self.bot.msg(self.bot.config.owner,
                         "Failed to send comment to bug %d" % bug)
 
-
-    def pulse_listener(self):
-        def got_message(data, message):
-            message.ack()
-
-            # Sanity checks
-            payload = data.get('payload')
-            if not payload:
-                return
-
-            change = payload.get('change')
-            if not change:
-                return
-
-            revlink = change.get('revlink')
-            if not revlink:
-                return
-
-            branch = change.get('branch')
-            if not branch:
-                return
-
-            rev = change.get('rev')
-            if not rev:
-                return
-
-            try:
-                properties = { a: b for a, b, c in
-                    change.get('properties', []) }
-            except:
-                properties = {}
-
-            change['files'] = ['...']
-            try:
-                if 'polled_moz_revision' in properties or \
-                        'polled_comm_revision' in properties or \
-                        'releng' not in data.get('_meta', {}) \
-                            .get('master_name', ''):
-                    self.bot.msg(self.bot.config.owner, "Ignored %s" % revlink)
-                    self.bot.msg(self.bot.config.owner,
-                        "Message data was: %s" % data, 10)
-                    return
-            except:
-                for line in traceback.format_exc().splitlines():
-                    self.bot.msg(self.bot.config.owner, line)
-
-            self.queue.put((rev, branch, revlink, data))
-
-        while not self.shutting_down:
-            # Connect to pulse
-            pulse = consumers.BuildConsumer(applabel=self.applabel, **self.auth)
-
-            # Tell pulse that you want to listen for all messages ('#' is
-            # everything) and give a function to call every time there is a
-            # message
-            pulse.configure(topic=['change.#'], callback=got_message)
-
-            # Manually do the work of pulse.listen() so as to be able to cleanly
-            # get out of it if necessary.
-            exchange = Exchange(pulse.exchange, type='topic')
-            queue = pulse._create_queue(exchange, pulse.topic[0])
-            consumer = pulse.connection.Consumer(queue, auto_declare=False,
-                callbacks=[pulse.callback])
-            consumer.queues[0].queue_declare()
-            # Bind to the first key.
-            consumer.queues[0].queue_bind()
-
-            with consumer:
-                while not self.shutting_down:
-                    try:
-                        pulse.connection.drain_events(timeout=1)
-                    except socket.timeout:
-                        pass
-                    except Exception as e:
-                        # If we failed for some other reason than the timeout,
-                        # cleanup and create a new connection.
-                        break
-
-            pulse.disconnect()
-
-
     def shutdown(self):
         self.shutting_down = True
+        self.pulse.shutdown()
         self.reporter_thread.join()
-        self.listener_thread.join()
         self.bugzilla_thread.join()
 
 
---
+++ pulsebot/pulse.py
@@ -0,0 +1,122 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import socket
+import threading
+from kombu import Exchange
+from mozillapulse import consumers
+from Queue import Queue, Empty
+
+
+class PulseListener(object):
+    def __init__(self, user=None, password=None, applabel=None):
+        self.shutting_down = False
+
+        if not applabel:
+            # Let's generate a unique label for the script
+            try:
+                import uuid
+                self.applabel = 'pulsebot-%s' % uuid.uuid4()
+            except:
+                from datetime import datetime
+                self = 'pulsebot-%s' % datetime.now()
+
+        self.auth = {
+            'user': user,
+            'password': password,
+        }
+
+        self.queue = Queue(42)
+        self.listener_thread = threading.Thread(target=self.pulse_listener)
+        self.listener_thread.start()
+
+    def pulse_listener(self):
+        def got_message(data, message):
+            message.ack()
+
+            # Sanity checks
+            payload = data.get('payload')
+            if not payload:
+                return
+
+            change = payload.get('change')
+            if not change:
+                return
+
+            revlink = change.get('revlink')
+            if not revlink:
+                return
+
+            branch = change.get('branch')
+            if not branch:
+                return
+
+            rev = change.get('rev')
+            if not rev:
+                return
+
+            try:
+                properties = {
+                    a: b for a, b, c
+                    in change.get('properties', [])
+                }
+            except:
+                properties = {}
+
+            change['files'] = ['...']
+            if ('polled_moz_revision' in properties or
+                    'polled_comm_revision' in properties or
+                    'releng' not in data.get('_meta', {})
+                    .get('master_name', '')):
+                return
+
+            self.queue.put((rev, branch, revlink, data))
+
+        while not self.shutting_down:
+            # Connect to pulse
+            pulse = consumers.BuildConsumer(
+                applabel=self.applabel, **self.auth)
+
+            # Tell pulse that you want to listen for all messages ('#' is
+            # everything) and give a function to call every time there is a
+            # message
+            pulse.configure(topic=['change.#'], callback=got_message)
+
+            # Manually do the work of pulse.listen() so as to be able to
+            # cleanly get out of it if necessary.
+            exchange = Exchange(pulse.exchange, type='topic')
+            queue = pulse._create_queue(exchange, pulse.topic[0])
+            consumer = pulse.connection.Consumer(
+                queue, auto_declare=False, callbacks=[pulse.callback])
+            consumer.queues[0].queue_declare()
+            # Bind to the first key.
+            consumer.queues[0].queue_bind()
+
+            with consumer:
+                while not self.shutting_down:
+                    try:
+                        pulse.connection.drain_events(timeout=1)
+                    except socket.timeout:
+                        pass
+                    except Exception:
+                        # If we failed for some other reason than the timeout,
+                        # cleanup and create a new connection.
+                        break
+
+            pulse.disconnect()
+
+    def shutdown(self):
+        if not self.shutting_down:
+            self.shutting_down = True
+            self.listener_thread.join()
+
+    def __iter__(self):
+        while not self.shutting_down:
+            try:
+                data = self.queue.get(timeout=1)
+            except Empty:
+                if not self.listener_thread.is_alive():
+                    self.shutdown()
+                continue
+            yield data
