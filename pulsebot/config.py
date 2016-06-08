a00b3939b9bbc8c71a28acd7119debf7b1fb953a
Mike Hommey <mh@glandium.org>
2016-06-08 11:53:28 +0900

    Add missing imports from previous commit

--- pulsebot/config.py
+++ pulsebot/config.py
@@ -2,7 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import fnmatch
 from sopel.config import Config as SopelConfig
+from collections import defaultdict
 
 
 class DispatchConfig(object):
